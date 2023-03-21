39e2adddc4f8699a783ce895781e39097e004be8
Maxime Millet <mmillet@iMac-de-Maxime.local>
2023-03-21 09:01:02 +0100

    .

--- setup.py
+++ setup.py
@@ -4,24 +4,27 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='pycalf',
-    version='0.1',
-    description='search calcyanin within a set of amino acid sequences',
+    name='pcalf',
+    packages=['pcalf'],
+    package_dir={'':'src'},
+    version='2.0',
+    description='Search calcyanin in a set of amino acid sequences',
     url='',
     author='Maxime Millet',
     author_email='maxime.luc.millet@gmail.com',
     license='MIT',
-    packages=find_packages(),
+    # packages=find_packages(),
     install_requires=[
-         "pyhmmer",
-         "biopython",
+         "pyhmmer==0.7.1",
+         "biopython==1.81",
          "numpy==1.22.4",
          "pyyaml==6.0",
-         "pandas",
+         "snakemake==7.22",
+         "pandas==1.5.3",
+         "tqdm==4.64.1",
     ],    
-    
     include_package_data=True,
-    scripts = [script for script in glob.glob("bin/*") if not os.path.basename(script).startswith("_") ],
+    scripts = [script for script in glob.glob("scripts/*") if not os.path.basename(script).startswith("_") ],
     py_modules = ["bin"],
     zip_safe=False
 )
